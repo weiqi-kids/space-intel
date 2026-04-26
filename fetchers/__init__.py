83327d04f6c1f95f487d2c4bd1693d697e202290
Light <lightman.chang@gmail.com>
2026-04-26 11:08:29 +0800

    feat: add Playwright fetchers for all tracked companies

--- fetchers/__init__.py
+++ fetchers/__init__.py
@@ -1,6 +1,37 @@
 """
-Company fetcher registry.
-Add Playwright-based fetchers here as they are implemented.
+公司新聞爬蟲
 """
 
-FETCHERS = {}
+from .base import CompanyFetcher, CompanyDocument
+
+from .ast_spacemobile import AstSpacemobileFetcher
+from .boeing import BoeingFetcher
+from .globalstar import GlobalstarFetcher
+from .intuitive import IntuitiveFetcher
+from .iridium import IridiumFetcher
+from .l3harris import L3harrisFetcher
+from .lockheed import LockheedFetcher
+from .northrop import NorthropFetcher
+from .planet import PlanetFetcher
+from .redwire import RedwireFetcher
+from .rocket_lab import RocketLabFetcher
+from .ses import SesFetcher
+from .spire import SpireFetcher
+from .viasat import ViasatFetcher
+
+FETCHERS = {
+    "ast_spacemobile": AstSpacemobileFetcher,
+    "boeing": BoeingFetcher,
+    "globalstar": GlobalstarFetcher,
+    "intuitive": IntuitiveFetcher,
+    "iridium": IridiumFetcher,
+    "l3harris": L3harrisFetcher,
+    "lockheed": LockheedFetcher,
+    "northrop": NorthropFetcher,
+    "planet": PlanetFetcher,
+    "redwire": RedwireFetcher,
+    "rocket_lab": RocketLabFetcher,
+    "ses": SesFetcher,
+    "spire": SpireFetcher,
+    "viasat": ViasatFetcher,
+}
